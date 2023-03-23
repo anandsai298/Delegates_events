bc0fb553dad36e0b469378b46a21bce1aa667ada
anandsaikumar <anandsai298@gmail.com>
2023-03-24 00:21:42 +0530

    [sai] Add : single delegate add method

---
+++ Delegates_Events/Delegates_Events/DelegateDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Events
+{
+    public delegate int Operation(int x,int y);
+    public class DelegateDemo
+    {
+        public int Add(int a,int b) 
+        {
+            return a + b;
+        }
+        public void Run()
+        {
+            Operation operation = new Operation(Add);
+            Console.WriteLine(operation(1, 2));
+        }
+    }
+}
---
+++ Delegates_Events/Delegates_Events/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Delegates_Events
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            DelegateDemo delegateDemo = new DelegateDemo();
+            delegateDemo.Run();
+        }
+    }
+}
